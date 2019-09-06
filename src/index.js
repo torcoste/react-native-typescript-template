35c4c592e92aa7da264d4ed211c8077ba961f383
Victor Kostyuk <torcoste@gmail.com>
2019-09-06 05:19:41 +0300

    YellowBox handler added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, YellowBox} from 'react-native';
 import App from './App';
 import {name as appName} from '../app.json';
+
+YellowBox.ignoreWarnings(['RCTBridge required dispatch_sync']);
 
 AppRegistry.registerComponent(appName, () => App);
