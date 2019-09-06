9330a2bf5020e42c9131c6190bc5192924a32cb8
Victor Kostyuk <torcoste@gmail.com>
2019-09-06 03:57:58 +0300

    [Experimental] Icon & Splash

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from '../app.json';
+// import SplashScreen from 'react-native-splash-screen';
 
+// SplashScreen.hide();
 AppRegistry.registerComponent(appName, () => App);
